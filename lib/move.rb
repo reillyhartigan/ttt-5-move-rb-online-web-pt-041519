afbb4fda79417350aee245c9fe98c7c62ee083e4
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-17 22:53:49 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,6 +9,6 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
-  user_input.to_i + 1 = converted_input
+  user_input.to_i = converted_input
   puts converted_input
 end
